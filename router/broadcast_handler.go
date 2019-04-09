14d25cfabc172852322de1e11d06fb21a17a6d34
altafan <pietro@vulpem.com>
2019-04-09 18:00:25 +0200

    fix endpoints to support liquid

--- router/broadcast_handler.go
+++ router/broadcast_handler.go
@@ -4,29 +4,46 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/vulpemventures/nigiri-chopsticks/helpers/liquidrpc"
+
 	log "github.com/sirupsen/logrus"
+	rpchelper "github.com/vulpemventures/nigiri-chopsticks/helpers/rpc"
 )
 
 // HandleBroadcastRequest forwards the request to the electrs HTTP server and mines a block if mining is enabled
 func (r *Router) HandleBroadcastRequest(res http.ResponseWriter, req *http.Request) {
-	r.HandleElectrsRequest(res, req)
+	/*
+		since our electrs version does not expose a broadcast endpoint for liquid, we need
+		to call the sendrawtransaction method of the underlying RPC daaemon instead
+	*/
+	if r.Config.Chain() == "liquid" {
+		url := r.Config.RPCServerURL()
+		tx := req.URL.Query().Get("tx")
+
+		status, txID, err := liquidrpc.Sendrawtransaction(url, tx)
+		if err != nil {
+			http.Error(res, err.Error(), status)
+			return
+		}
+
+		json.NewEncoder(res).Encode(map[string]string{"txId": txID})
+	} else {
+		r.HandleElectrsRequest(res, req)
+	}
 
 	if r.Config.IsMiningEnabled() {
-		url := r.Config.RPCServerURL()
-		body := `{"jsonrpc":"1.0", "id": "2", "method":"generate", "params":[1]}`
-		status, resp, err := post(url, body, nil)
+		mineOneBlock(r)
+	}
+}
+
+func mineOneBlock(r *Router) {
+	url := r.Config.RPCServerURL()
+	_, blockHash, err := rpchelper.Mine(url, 1)
+	if r.Config.IsLoggerEnabled() {
 		if err != nil {
 			log.WithError(err).Warning("Error while mining a block")
-		}
-		if status != http.StatusOK {
-			log.WithFields(log.Fields{
-				"response": resp,
-				"status":   status,
-			}).Warning("Error while mining a block")
 		} else {
-			out := map[string]string{}
-			json.Unmarshal([]byte(resp), &out)
-			log.WithField("block_hash", out["result"]).Info("Block mined")
+			log.WithField("block_hash", blockHash[0]).Info("Block mined")
 		}
 	}
 }
--- router/faucet_handler.go
+++ router/faucet_handler.go
@@ -3,25 +3,42 @@ package router
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-// HandleFaucetRequest sends funds to the address passed in the request body
+// HandleFaucetRequest sends funds to the address passed in the request body by
+// retrieving the signed transaction from the faucet and broadcasting via the
+// electrs broadcast endpoint
 func (r *Router) HandleFaucetRequest(res http.ResponseWriter, req *http.Request) {
 	body := parseRequestBody(req.Body)
 
-	status, signedTx, err := r.Faucet.NewTransaction(body["address"])
+	status, tx, err := r.Faucet.NewTransaction(body["address"])
 	if err != nil {
 		http.Error(res, err.Error(), status)
 		return
 	}
 
-	url := fmt.Sprintf("%s/broadcast?tx=%s", r.Config.ElectrsURL(), signedTx)
-	status, resp, err := get(url, nil)
-	if err != nil {
-		http.Error(res, err.Error(), status)
+	/*
+		since the liquid implementation of Faucet uses the sendtoaddress RPC method,
+		it already returns the hash of the transaction, then we just need to get it
+		confirmed mining one block and returning the tx hash in the response body
+	*/
+	if r.Config.Chain() == "liquid" {
+		mineOneBlock(r)
+		json.NewEncoder(res).Encode(map[string]string{"txId": tx})
 		return
 	}
 
-	json.NewEncoder(res).Encode(map[string]string{"txId": resp})
+	broadcastRequest, _ := http.NewRequest("GET", fmt.Sprintf("%s/broadcast?tx=%s", r.Config.ElectrsURL(), tx), nil)
+
+	r.HandleBroadcastRequest(res, broadcastRequest)
+}
+
+func parseRequestBody(body io.ReadCloser) map[string]string {
+	decoder := json.NewDecoder(body)
+	var decodedBody map[string]string
+	decoder.Decode(&decodedBody)
+
+	return decodedBody
 }
