035ca87a788931fdf7ff39f95071d569d19980ad
altafan <pietro@vulpem.com>
2019-04-09 18:01:03 +0200

    fund faucet wallet at start time if balance is 0

--- router/main.go
+++ router/main.go
@@ -2,8 +2,10 @@ package router
 
 import (
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 	cfg "github.com/vulpemventures/nigiri-chopsticks/config"
 	"github.com/vulpemventures/nigiri-chopsticks/faucet"
+	"github.com/vulpemventures/nigiri-chopsticks/faucet/liquid"
 	"github.com/vulpemventures/nigiri-chopsticks/faucet/regtest"
 	"github.com/vulpemventures/nigiri-chopsticks/router/middleware"
 )
@@ -15,6 +17,11 @@ type Router struct {
 	Faucet faucet.Faucet
 }
 
+var faucets = map[string]func(chain string) faucet.Faucet{
+	"bitcoin": regtestfaucet.NewFaucet,
+	"liquid":  liquidfaucet.NewFaucet,
+}
+
 // NewRouter returns a new Router instance
 func NewRouter(config cfg.Config) *Router {
 	router := mux.NewRouter().StrictSlash(true)
@@ -23,14 +30,24 @@ func NewRouter(config cfg.Config) *Router {
 
 	if r.Config.IsFaucetEnabled() {
 		url := r.Config.RPCServerURL()
-		r.Faucet = regtestfaucet.NewFaucet(url)
-		r.HandleFunc("/faucet", r.HandleFaucetRequest).Methods("POST")
+		chain := r.Config.Chain()
+		r.Faucet = faucets[chain](url)
+		r.HandleFunc("/faucet", r.HandleFaucetRequest).Queries("tx", "").Methods("POST")
+
+		status, blockHashes, err := r.Faucet.Fund()
+		if err != nil {
+			log.WithError(err).WithField("status", status).Warning("Could not be able to fund faucet, please do it manually")
+		} else {
+			if len(blockHashes) > 0 {
+				log.WithField("num_blocks", len(blockHashes)).Info("Faucet has been funded mining some blocks")
+			}
+		}
 	}
 
 	if config.IsLoggerEnabled() {
 		r.Use(middleware.Logger)
 	}
-	r.HandleFunc("/tx", r.HandleBroadcastRequest).Methods("POST")
+	r.HandleFunc("/broadcast", r.HandleBroadcastRequest).Methods("POST")
 	r.PathPrefix("/").HandlerFunc(r.HandleElectrsRequest)
 
 	return r
