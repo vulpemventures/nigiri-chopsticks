0c9929ce6d640d3df410f5a878cf97717eaf58fb
altafan <pietro@vulpem.com>
2019-04-18 15:11:36 +0200

    remove liquid broadcast endpoint workaround

--- router/broadcast_handler.go
+++ router/broadcast_handler.go
@@ -9,28 +9,7 @@ import (
 
 // HandleBroadcastRequest forwards the request to the electrs HTTP server and mines a block if mining is enabled
 func (r *Router) HandleBroadcastRequest(res http.ResponseWriter, req *http.Request) {
-	/*
-		since our electrs version does not expose a broadcast endpoint for liquid, we need
-		to call the sendrawtransaction method of the underlying RPC daaemon instead
-	*/
-	if r.Config.Chain() == "liquid" {
-		tx := req.URL.Query().Get("tx")
-
-		status, resp, err := r.RPCClient.Call("sendrawtransaction", []interface{}{tx})
-		if err != nil {
-			http.Error(res, err.Error(), status)
-			return
-		}
-
-		var txId string
-		if err = json.Unmarshal(resp.Result, &txId); err != nil {
-			http.Error(res, err.Error(), http.StatusInternalServerError)
-		}
-
-		json.NewEncoder(res).Encode(map[string]string{"txId": txId})
-	} else {
-		r.HandleElectrsRequest(res, req)
-	}
+	r.HandleElectrsRequest(res, req)
 
 	if r.Config.IsMiningEnabled() {
 		mineOneBlock(r)
