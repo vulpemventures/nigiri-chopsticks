815f040172834122a3d0b705d1e6b4f644d0f172
altafan <pietro@vulpem.com>
2019-05-22 18:08:31 +0200

    fix num blocks mined per chain and log response

--- router/broadcast_handler.go
+++ router/broadcast_handler.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // HandleBroadcastRequest forwards the request to the electrs HTTP server and mines a block if mining is enabled
@@ -9,6 +11,17 @@ func (r *Router) HandleBroadcastRequest(res http.ResponseWriter, req *http.Reque
 	r.HandleElectrsRequest(res, req)
 
 	if r.Config.IsMiningEnabled() {
-		r.Faucet.Mine(1)
+		blocks := 1
+		if r.Config.Chain() == "liquid" {
+			blocks = 10
+		}
+		status, blockHashes, err := r.Faucet.Mine(blocks)
+		if err != nil {
+			log.WithError(err).WithField("status", status).Warning("An unexpected error occured while mining blocks")
+		} else {
+			if r.Config.IsLoggerEnabled() {
+				log.WithField("blocks mined", len(blockHashes)).Info("Transaction has been confirmed")
+			}
+		}
 	}
 }
