a69ab5ba446efb138b632dafa1451e64934b41c5
altafan <pietro@vulpem.com>
2020-04-22 20:28:10 +0200

    Use scribble for registry

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.12
 
 require (
 	github.com/gorilla/mux v1.7.0
+	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/sdomino/scribble v0.0.0-20191024200645-4116320640ba
 	github.com/sirupsen/logrus v1.4.0
 	golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a
 	golang.org/x/sys v0.0.0-20190312061237-fead79001313 // indirect
--- helpers/registry.go
+++ helpers/registry.go
@@ -4,40 +4,84 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"strings"
+
+	"github.com/sdomino/scribble"
 )
 
 // Registry handles writing/reading json file where are stored info about issued asset
 type Registry struct {
-	path string
+	db *scribble.Driver
 }
 
 // NewRegistry returns a new Registry of error if the path is not absolute
-func NewRegistry(path string) *Registry {
-	r := &Registry{path}
-	return r
+func NewRegistry(path string) (*Registry, error) {
+	r := &Registry{}
+	db, err := scribble.New(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	r.db = db
+	return r, nil
 }
 
 // AddEntry adds an entry to the register by previously making sure that
 // the incoming entry does not already exist in the registry
 func (r *Registry) AddEntry(asset string, issuanceInput map[string]interface{}, contract map[string]interface{}) error {
-	registry, err := r.load()
-	if err != nil {
+	entry := map[string]interface{}{}
+	err := r.db.Read("registry", asset, &entry)
+	if err != nil && !strings.Contains(err.Error(), "no such file or directory") {
 		return err
 	}
 
-	_, ok := registry[asset]
-	if ok {
+	if len(entry) != 0 {
 		return errors.New("Asset already exists on registry")
 	}
-	entry := map[string]interface{}{
+
+	entry = map[string]interface{}{
 		"asset":         asset,
 		"issuance_txin": issuanceInput,
 		"contract":      contract,
 		"name":          contract["name"].(string),
 		"ticker":        contract["ticker"].(string),
 	}
-	registry[asset] = entry
-	return r.save(registry)
+	return r.db.Write("registry", asset, entry)
+}
+
+// GetEntry returns and entry if it exist in registry or NIL
+func (r *Registry) GetEntry(asset string) (interface{}, error) {
+	entry := map[string]interface{}{}
+	err := r.db.Read("registry", asset, &entry)
+	if err != nil {
+		return nil, err
+	}
+
+	return entry, nil
+}
+
+func (r *Registry) GetEntries(assets []interface{}) ([]interface{}, error) {
+	entries := []interface{}{}
+
+	if len(assets) == 0 {
+		records, err := r.db.ReadAll("registry")
+		if err != nil {
+			return nil, err
+		}
+
+		for _, f := range records {
+			var entry interface{}
+			json.Unmarshal([]byte(f), &entry)
+			entries = append(entries, entry)
+		}
+	} else {
+		for _, asset := range assets {
+			var entry interface{}
+			r.db.Read("registry", asset.(string), &entry)
+			entries = append(entries, entry)
+		}
+	}
+
+	return entries, nil
 }
 
 func (r *Registry) load() (map[string]interface{}, error) {
