d3194c71a8d14b5a228f13505bb5f3f5af9193a1
altafan <pietro@vulpem.com>
2020-04-24 13:03:59 +0200

    Add tests for /asset/:asset_id endpoint

---
+++ router/electrs_handler_test.go
@@ -0,0 +1,72 @@
+package router
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestAssetEndpointWithExtraInfo(t *testing.T) {
+	r := NewTestRouter(withLiquid)
+	resp := mintRequest(r, liquidAddress, assetQuantity, name, ticker)
+
+	parsedResp := map[string]interface{}{}
+	json.Unmarshal(resp.Body.Bytes(), &parsedResp)
+	asset := parsedResp["asset"].(string)
+
+	time.Sleep(5 * time.Second)
+
+	resp = assetRequest(r, asset)
+	if resp.Code != http.StatusOK {
+		t.Fatalf("Expected status %d, got: %d\n", http.StatusOK, resp.Code)
+	}
+
+	parsedResp = map[string]interface{}{}
+	json.Unmarshal(resp.Body.Bytes(), &parsedResp)
+	resName := parsedResp["name"].(string)
+	resTicker := parsedResp["ticker"].(string)
+
+	if strings.Compare(name, resName) != 0 {
+		t.Fatalf("Expected asset name: %s, got: %s\n", name, resName)
+	}
+	if strings.Compare(ticker, resTicker) != 0 {
+		t.Fatalf("Expected asset ticker: %s, got: %s\n", ticker, resTicker)
+	}
+}
+
+func TestAssetEndpointWithoutExtraInfo(t *testing.T) {
+	r := NewTestRouter(withLiquid)
+	resp := mintRequest(r, liquidAddress, assetQuantity, nil, nil)
+
+	parsedResp := map[string]interface{}{}
+	json.Unmarshal(resp.Body.Bytes(), &parsedResp)
+	asset := parsedResp["asset"].(string)
+
+	time.Sleep(5 * time.Second)
+
+	resp = assetRequest(r, asset)
+	if resp.Code != http.StatusOK {
+		t.Fatalf("Expected status %d, got: %d\n", http.StatusOK, resp.Code)
+	}
+
+	parsedResp = map[string]interface{}{}
+	json.Unmarshal(resp.Body.Bytes(), &parsedResp)
+
+	if parsedResp["name"] != nil {
+		t.Fatalf("Asset name should not be defined")
+	}
+
+	if parsedResp["ticker"] != nil {
+		t.Fatalf("Asset ticker should not be defined")
+	}
+}
+
+func assetRequest(r *Router, asset interface{}) *httptest.ResponseRecorder {
+	path := fmt.Sprintf("/asset/%s", asset)
+	req, _ := http.NewRequest("GET", path, nil)
+	return doRequest(r, req)
+}
